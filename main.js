721580a9a61097eb1286cbbc3d753abea4ed1eee
Gayangamage <gayangamage.se@gmail.com>
2022-05-16 23:48:59 +0200

    remove dc webhook url

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 const { MessageEmbed, WebhookClient } = require('discord.js');
 
 /* url: discord webhook url */
-const webhookClient = new WebhookClient({ url: 'https://discord.com/api/webhooks/975777851112554496/cq0wV_65W7L1PWLCpVjgjASp0D1NkAxAtKqd7-9-vM4uXkTxB_tpGcfxyqY4vvYpvNno' });
-
-
-
+const webhookClient = new WebhookClient({ url: '/* discord webhook url */' });
 const embed = new MessageEmbed()
 	.setTitle('Test Title')
-    .setImage("https://res.cloudinary.com/dvclmgswx/image/upload/v1649458733/Screen-Shot-2020-07-10-at-10.24.04-AM_b8vux5.png")
+    .setImage("image url")
 	.setColor('#33FF39');
 
 webhookClient.send({
